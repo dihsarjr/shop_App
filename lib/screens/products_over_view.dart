0aca5583fb9336b5f3b1983f9b4b2df062e39e67
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-13 22:34:51 +0530

    dummyDataAdded

--- lib/screens/products_over_view.dart
+++ lib/screens/products_over_view.dart
@@ -2,7 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:shop_app/model/product_model.dart';
 
 class ProductsOverView extends StatelessWidget {
-  final List<ProductModel> loadedProducts =[];
+  final List<ProductModel> loadedProducts = [
+    ProductModel(
+      id: 'p1',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+      'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    ),
+    ProductModel(
+      id: 'p2',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+      'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    ),
+    ProductModel(
+      id: 'p3',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+      'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    ),
+    ProductModel(
+      id: 'p4',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+      'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Container();
