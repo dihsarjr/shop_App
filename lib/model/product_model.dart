47479f0ace7542784bc346ed5c03c6cfd796e09f
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-13 22:25:04 +0530

    modelCreated

---
+++ lib/model/product_model.dart
@@ -0,0 +1,9 @@
+class ProductModel {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool favorite;
+  ProductModel({this.id,this.title,this.description,this.price,this.imageUrl,this.favorite});
+}
