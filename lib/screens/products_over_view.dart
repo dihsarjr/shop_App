8d7f7a2e4e02f421abcc1515306f102bd5ade14b
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-07-13 22:31:53 +0530

    ListCreated

--- lib/screens/products_over_view.dart
+++ lib/screens/products_over_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/model/product_model.dart';
 
 class ProductsOverView extends StatelessWidget {
+  final List<ProductModel> loadedProducts =[];
   @override
   Widget build(BuildContext context) {
     return Container();
